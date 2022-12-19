8eeb7f7691f1544072506e52a06550fb2af85590
alexisOclock <alexis@oclock.io>
2022-12-19 11:01:26 +0100

    étape 3

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,10 +1,11 @@
 // objet qui contient tout ce qui est lié à l'utilisation des listes
 const list = {
+  addModal: document.getElementById('addListModal'),
   addListenerToActions: function() {
     // Ouverture de la modale au clic sur le bouton
     const addListButton = document.getElementById('addListButton');
     addListButton.addEventListener('click', list.showAddModal);
-    
+
     // Fermeture au clic sur les boutons
     const closeButtons = document.querySelectorAll('.close');
     // for (const button of closeButtons) {
@@ -13,13 +14,24 @@ const list = {
     closeButtons.forEach((button) => {
       button.addEventListener('click', list.hideModal);
     });
+
+    // création d'une liste à la soumission du formulaire
+    // la méthode querySelector existe sur le document mais aussi sur un Element pour chercher dans une zone restreinte
+    list.form = list.addModal.querySelector('form');
+    list.form.addEventListener('submit', list.handleAddForm);
   },
   showAddModal: function() {
-    const addModal = document.getElementById('addListModal');
-    addModal.classList.add('is-active');
+    list.addModal.classList.add('is-active');
   },
   hideModal: function() {
-    const addModal = document.getElementById('addListModal');
-    addModal.classList.remove('is-active');
+    list.addModal.classList.remove('is-active');
+  },
+  handleAddForm: function(event) {
+    event.preventDefault();
+    // on construit un objet contenant toutes les données du formulaires
+    const formData = new FormData(list.form);
+    // on peut accéder à une donnée via la méthode get à qui on passe en argument
+    // le nom / la clé de la valeur à récupérer
+    const listName = formData.get('name');
   },
 };
